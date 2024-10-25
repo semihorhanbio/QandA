46aade5630f9e0667a1853b92b32a366931e35d7
Semih Orhan <sorhan3.14@gmail.com>
2024-10-25 15:02:57 +0300

    use children props

---
+++ frontend/src/components/Page.tsx
@@ -0,0 +1,11 @@
+import { PageTitle } from './PageTitle';
+interface Props {
+  title?: string;
+  children: React.ReactNode;
+}
+export const Page = ({ title, children }: Props) => (
+  <div>
+    {title && <PageTitle>{title}</PageTitle>}
+    {children}
+  </div>
+);
---
+++ frontend/src/components/PageTitle.tsx
@@ -0,0 +1,4 @@
+interface Props {
+  children: React.ReactNode;
+}
+export const PageTitle = ({ children }: Props) => <h2>{children}</h2>;
