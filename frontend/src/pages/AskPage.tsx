a5efbf3d358dc48a13d2a81f66b274ee8422ed4c
Semih <50242428+semihorhanbio@users.noreply.github.com>
2024-10-30 15:07:04 +0300

    implement forms

--- frontend/src/pages/AskPage.tsx
+++ frontend/src/pages/AskPage.tsx
@@ -1,4 +1,48 @@
 import { Page } from '../components/Page';
+import {
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldInput,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+} from '../Styles';
+import { useForm } from 'react-hook-form';
 
-export const AskPage = () => <Page title="Ask a question">{null}</Page>;
+export const AskPage = () => {
+  const { register } = useForm<FormData>();
+  return (
+    <Page title="Ask a question">
+      <form>
+        <Fieldset>
+          <FieldContainer>
+            <FieldLabel htmlFor="title">Title</FieldLabel>
+            <FieldInput
+              {...register('title')}
+              id="title"
+              name="title"
+              type="text"
+            />
+          </FieldContainer>
+          <FieldContainer>
+            <FieldLabel htmlFor="content">Content</FieldLabel>
+            <FieldTextArea
+              {...register('content')}
+              id="content"
+              name="content"
+            />
+          </FieldContainer>
+        </Fieldset>
+        <FormButtonContainer>
+          <PrimaryButton type="submit">Submit Your Question</PrimaryButton>
+        </FormButtonContainer>
+      </form>
+    </Page>
+  );
+};
 export default AskPage;
+type FormData = {
+  title: string;
+  content: string;
+};
--- frontend/src/pages/QuestionPage.tsx
+++ frontend/src/pages/QuestionPage.tsx
@@ -1,69 +1,105 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import { gray3, gray6 } from '../Styles';
-import { useState, useEffect } from 'react';
+import {
+  gray3,
+  gray6,
+  Fieldset,
+  FieldContainer,
+  FieldLabel,
+  FieldTextArea,
+  FormButtonContainer,
+  PrimaryButton,
+} from '../Styles';
+import {useState, useEffect} from 'react';
 import { Page } from '../components/Page';
-import { AnswerList } from '../components/AnswerList';
 import { useParams } from 'react-router-dom';
 import { QuestionData, getQuestion } from '../QuestionsData';
-
+import { AnswerList } from '../components/AnswerList';
+import { useForm } from 'react-hook-form';
+type FormData = {
+  content: string;
+};
 export const QuestionPage = () => {
-    const [question, setQuestion] = useState<QuestionData | null>(null);
-    const { questionId } = useParams();
-    useEffect(() => {
-      const doGetQuestion = async (questionId: number) => {
-        const foundQuestion = await getQuestion(questionId);
-        setQuestion(foundQuestion);
-      };
-      if (questionId) {
-        doGetQuestion(Number(questionId));
-      }
-    }, [questionId]);
-    return (
-      <Page>
+  const [question, setQuestion] = useState<QuestionData | null>(null);
+  const { questionId } = useParams();
+  
+  useEffect(() => {
+    const doGetQuestion = async (questionId: number) => {
+      const foundQuestion = await getQuestion(questionId);
+      setQuestion(foundQuestion);
+    };
+    if (questionId) {
+      doGetQuestion(Number(questionId));
+    }
+  }, [questionId]);
+  const { register } = useForm<FormData>();
+  return (
+    <Page>
+      <div
+        css={css`
+          background-color: white;
+          padding: 15px 20px 20px 20px;
+          border-radius: 4px;
+          border: 1px solid ${gray6};
+          box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+        `}
+      >
         <div
           css={css`
-            background-color: white;
-            padding: 15px 20px 20px 20px;
-            border-radius: 4px;
-            border: 1px solid ${gray6};
-            box-shadow: 0 3px 5px 0 rgba(0, 0, 0, 0.16);
+            font-size: 19px;
+            font-weight: bold;
+            margin: 10px 0px 5px;
           `}
         >
-          <div
-            css={css`
-              font-size: 19px;
-              font-weight: bold;
-              margin: 10px 0px 5px;
-            `}
-          >
-            {question === null ? '' : question.title}
-          </div>
-          {question && (
-            <>
-              <p
-                css={css`
-                  margin-top: 0px;
-                  background-color: white;
-                `}
-              >
-                {question.content}
-              </p>
-              <div
-                css={css`
-                  font-size: 12px;
-                  font-style: italic;
-                  color: ${gray3};
-                `}
-              >
-                {`Asked by ${question.userName} on
-    ${question.created.toLocaleDateString()}
-    ${question.created.toLocaleTimeString()}`}
-              </div>
-              <AnswerList data={question.answers} />
-            </>
-          )}
+          {question === null ? '' : question.title}
         </div>
-      </Page>
-    );
-  };
+        {question !== null && (
+          <>
+            <p
+              css={css`
+                margin-top: 0px;
+                background-color: white;
+              `}
+            >
+              {question.content}
+            </p>
+            <div
+              css={css`
+                font-size: 12px;
+                font-style: italic;
+                color: ${gray3};
+              `}
+            >
+              {`Asked by ${question.userName} on
+  ${question.created.toLocaleDateString()}
+  ${question.created.toLocaleTimeString()}`}
+            </div>
+            <AnswerList data={question.answers} />
+            <form
+              css={css`
+                margin-top: 20px;
+              `                       
+            }              
+            >
+              <Fieldset>
+                <FieldContainer>
+                  <FieldLabel htmlFor="content">Your Answer</FieldLabel>
+                  <FieldTextArea
+                    {...register('content')}
+                    id="content"
+                    name="content"
+                  />
+                </FieldContainer>
+                <FormButtonContainer>
+                  <PrimaryButton type="submit">
+                    Submit Your Answer
+                  </PrimaryButton>
+                </FormButtonContainer>
+              </Fieldset>
+            </form>
+          </>
+        )}
+      </div>
+    </Page>
+  );
+};
